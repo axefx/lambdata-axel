5a9d267c0a21e0e1be2bece9b54503fc5964251a
axefx <axellfx@gmail.com>
2020-04-08 13:37:10 -0700

    updated version number to upload

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="lambdata_axel", # the name that you will install via pip
-    version="1.0",
+    name="lambdata_axel",  # the name that you will install via pip
+    version="1.1",
     author="Axel Corro",
     author_email="axacorro@gmail.com",
     description="Some data science tools",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
+    # required if using a md file for long desc
+    long_description_content_type="text/markdown",
+    # license="MIT",
     url="https://github.com/axefx/lambdata-axel",
-    #keywords="",
-    packages=find_packages() # ["my_lambdata"]
+    # keywords="",
+    packages=find_packages()  # ["my_lambdata"]
 )
