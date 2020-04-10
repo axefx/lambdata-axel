a1210104d47adc9b391727a3fbe3e86af8848391
axefx <axellfx@gmail.com>
2020-04-09 18:03:21 -0700

    updated version and license

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata_axel",  # the name that you will install via pip
-    version="1.2.0",
+    version="1.2.1",
     author="Axel Corro",
     author_email="axacorro@gmail.com",
     description="Some data science tools",
     long_description=long_description,
     # required if using a md file for long desc
     long_description_content_type="text/markdown",
-    # license="MIT",
+    license="MIT",
     url="https://github.com/axefx/lambdata-axel",
     # keywords="",
     packages=find_packages()  # ["my_lambdata"]
